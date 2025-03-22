03e2321424ff432fe525ac86ab69ef15b7ca7ab1
Sagnik Goswami <sagnik.gos06@gmail.com>
2025-03-22 12:25:18 +0530

    Update settings.py

--- django-learn/backend/backend/settings.py
+++ django-learn/backend/backend/settings.py
@@ -131,6 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
+    "https://django-crud-rose.vercel.app",
     # Allow React frontend to access the backend
 ]
 
